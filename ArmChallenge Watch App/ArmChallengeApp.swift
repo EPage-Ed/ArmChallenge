e4c217029d96f130932e23818efbc98365bf058d
Ed Arenberg <arenberg@epage.com>
2023-04-18 15:29:26 -0700

    Initial Commit

---
+++ ArmChallenge Watch App/ArmChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmChallengeApp.swift
+//  ArmChallenge Watch App
+//
+//  Created by Edward Arenberg on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ArmChallenge_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
